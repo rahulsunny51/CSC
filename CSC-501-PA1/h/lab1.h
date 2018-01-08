60c705002894c551a694a51b2b5c88722f33b6ec
rahulsunny51 <31579424+rahulsunny51@users.noreply.github.com>
2018-01-07 23:10:04 -0500

    Add files via upload

---
+++ CSC-501-PA1/h/lab1.h
@@ -0,0 +1,40 @@
+#define EXPDISTSCHED 1
+#define LINUXSCHED 2
+
+
+#ifndef _setschedclass_h
+#define _setschedclass_h
+
+void setschedclass(int);
+
+#endif
+
+#ifndef _setschedclass_h
+#define _setschedclass_h
+
+int getschedclass();
+
+#endif
+
+#ifndef _pow_h
+#define _pow_h
+
+
+double pow(double , int );
+
+#endif
+
+#ifndef _log_h
+#define _log_h
+
+double log(double );
+
+#endif
+
+#ifndef _expdev_h
+#define _expdev_h
+
+double expdev(double );
+
+#endif
+
